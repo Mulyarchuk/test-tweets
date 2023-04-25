758140445e4992b5d61445ebde1142cf08c4b5ea
Mulyarchuk Elena <ml.mulyarchuk@gmail.com>
2023-04-25 12:27:32 +0200

    finish1

--- src/components/CardsList/CardsList.jsx
+++ src/components/CardsList/CardsList.jsx
@@ -3,9 +3,11 @@ import Cards from 'components/Cards/Cards';
 import LoadMoreBtn from 'components/LoadMore/LoadMore'
 import user from 'data/user.json';
 import { CardsList } from './CardList.styled';
+import { useLocation, Link } from 'react-router-dom';
 
 
 const CardList = () => {
+  const location = useLocation();
   const [visible, setVisible] = useState(6);
   const handleLoadMore = ()=>{
     setVisible((prevValue)=> prevValue + 6)
@@ -54,6 +56,20 @@ const CardList = () => {
  
   return (
     <>
+    <Link
+        style={{
+          padding: `14px 28px`,
+          color: `#373737`,
+          backgroundColor:`#5cd3a8`,
+          borderRadius: `8px`,
+          boxShadow: `1px 1px 7px 1px #8ba8bc`,
+          transition: `transform 250ms linear`,
+        }}
+        to="/"
+        state={{ from: location }}
+      >
+        BACK
+      </Link>
     <CardsList>
       {users.slice(0, visible).map(user => (
         <Cards key={user.id} user={user} changeData={changeData} />
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <section>
-      <h1>Welcome!</h1>
-      <p>
-        Click 
-        <Link to="/tweets">here</Link>
-        to see tweets
+      <h1>Wellcome!</h1>
+      <p> 
+        <Link 
+        style={{
+            padding: `14px 28px`,
+            color: `#373737`,
+            backgroundColor:`#5cd3a8`,
+            borderRadius: `8px`,
+            boxShadow: `1px 1px 7px 1px #8ba8bc`,
+            transition: `transform 250ms linear`,
+          }}
+        to="/tweets">TWEETS</Link>
         </p>
     </section>
   );
